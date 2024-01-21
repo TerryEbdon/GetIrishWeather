dacd56fca353c8ca62488fd6c372b9a354c2b92d
Terry Ebdon <47636739+TerryEbdon@users.noreply.github.com>
2024-01-21 23:45:52 +0000

    Fix hour handling

--- src/main/groovy/net/ebdon/getirishweather/App.groovy
+++ src/main/groovy/net/ebdon/getirishweather/App.groovy
@@ -4,17 +4,17 @@ import groovy.ant.AntBuilder
 
 class IrishWeather {
   String run() {
-    String rssFileName = 'rss.xml'
+    final String rssFileName = 'rss.xml'
     final String rssUrl = 'https://audioboom.com/channels/5003380.rss'
     AntBuilder ant = new AntBuilder()
     ant.get( src: rssUrl, dest: rssFileName, usetimestamp: true, quiet: true )
     def rss = new XmlParser().parse(
       new File( rssFileName )
     )
-    final String mp3Url         = rss.channel.item[0].enclosure.@url[0]
-    final String title          = rss.channel.item[0].title.text()
+    final String mp3Url   = rss.channel.item[0].enclosure.@url[0]
+    final String title    = rss.channel.item[0].title.text()
 
-    writePubDatesFile( rss )
+    // writePubDatesFile( rss )
 
     final String mp3FileName = buildMp3FileName( cleanTitle( title ))
     println mp3Url
@@ -27,21 +27,24 @@ class IrishWeather {
     def fnBits        = title.split()
     String year       = fnBits[-1]
     int month         = getMonthNumber( fnBits[-2] )
-    int hour          = Integer.parseInt(fnBits[0] - 'am' - 'pm' )
-    hour              = fnBits[0] == '6pm' ? 18 : hour
     int day           = Integer.parseInt(fnBits[-3])
     String dayName    = fnBits[1].toUpperCase()[0..2]
+    int hour          = Integer.parseInt(fnBits[0] - 'am' - 'pm' )
+
+    if ( fnBits[0].toLowerCase().contains('pm') && hour != 12 ) {
+      hour += 12
+    }
 
     String.format( '%4s-%02d-%02d_%02d00_%s.mp3',
         year, month, day, hour, dayName )
   }
 
   private final int getMonthNumber(String monthName) {
-      return java.time.Month.valueOf(monthName.toUpperCase()).getValue();
+    return java.time.Month.valueOf(monthName.toUpperCase()).getValue()
   }
 
   final String cleanTitle( String title ) {
-    final String mp3FileNameRegex = /([0-9]{3}): Ireland's Weather /
+    final String mp3FileNameRegex = /([0-9]+): Ireland's Weather /
     title.replaceAll( mp3FileNameRegex, '' )
   }
 
@@ -59,6 +62,3 @@ class IrishWeather {
     new IrishWeather().run()
   }
 }
-
-// w:https://audioboom.com/posts/8437173-ireland-s-weather-6am-saturday-20-january-2024.mp3?download=1
-// g:https://audioboom.com/posts/8437173.mp3?modified=1705729925&sid=5003380&source=rss
